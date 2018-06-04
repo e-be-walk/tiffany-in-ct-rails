226e7e3e482dd56a28a9136fa9233d6a2eac89fb
e-be-walk <e.be.walk@gmail.com>
2018-06-04 16:14:15 -0400

    Adding signup page for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
-  has_many :comments
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = User.create(user_params)
+      session[:user_id] = @user.id
+      raise params.inspect
+      redirect_to user_path(@user)
+    else
+      redirect_to '/'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :site_admin)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'welcome#home'
+  
   resources :windows
   resources :sites
   resources :users
