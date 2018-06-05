4d0d437a508f125905d76c192c241b4bb0671bca
e-be-walk <e.be.walk@gmail.com>
2018-06-05 16:41:54 -0400

    Working on sites views and controllers

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,29 @@
 class SitesController < ApplicationController
-  has_many :windows
-  has_many :comments, through: :users
+
+  def show
+  end
+
+  def new
+    @site = Site.new
+  end
+
+  def create
+    @site = Site.new(site_params)
+    redirect_to site_path(@user, @site)
+  end
+
+  private
+
+  def site_params
+    params.require(:site).permit(
+      :name,
+      :street_address,
+      :city,
+      :zip_code,
+      :contact_info,
+      :accessibility,
+      :site_info,
+      :image
+    )
+  end
 end
